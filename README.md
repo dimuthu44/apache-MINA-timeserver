f1a660236bc5f269b777a2e9da5ab959659199b3
DimuthuJ <dimuthu.jayamanna@pagero.com>
2023-04-25 15:32:17 +0200

    Using Apache MINA IOHandlerAdapter as a timeserver

---
+++ README.md
@@ -0,0 +1,4 @@
+**Usage** 
+: Open a terminal and issue - _telnet 127.0.0.1 9124_
+: Type something and it will get printed in program logs
+
---
+++ src/main/java/dimuthu/och/company/App.java
@@ -0,0 +1,29 @@
+package dimuthu.och.company;
+
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
+public class App {
+    private static final int PORT = 9124;
+
+    public static void main(String[] args) throws IOException {
+        IoAcceptor acceptor = new NioSocketAcceptor();
+
+        acceptor.getFilterChain().addLast("logger", new LoggingFilter());
+        acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory(StandardCharsets.UTF_8)));
+        acceptor.setHandler(new TimeServerHandler());
+
+        acceptor.getSessionConfig().setReadBufferSize(2048);
+        acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
+
+        acceptor.bind(new InetSocketAddress(PORT));
+    }
+}
